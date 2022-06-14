ae74057927138b6e894dc088133783ca9fb22e1b
Niurka Carrión <niurka.carrion@pucp.edu.pe>
2022-06-14 00:14:25 -0500

    servlet

--- LAB8_IWEB1_BASE/src/main/java/Servlets/CancionesServlet.java
+++ LAB8_IWEB1_BASE/src/main/java/Servlets/CancionesServlet.java
@@ -1,9 +1,15 @@
 package Servlets;
 
+import Beans.Cancion;
+import Beans.Tour;
+import Daos.CancionesDao;
+import Daos.TourDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "CancionesServlet", value = "/listaCanciones")
 public class CancionesServlet extends HttpServlet {
@@ -14,6 +20,14 @@ public class CancionesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        CancionesDao cancionesDao = new CancionesDao();
+        ArrayList<Cancion> listaCanciones = cancionesDao.obtenerListaCanciones();
+
+        request.setAttribute("listaCanciones",listaCanciones);
+
+        RequestDispatcher view =request.getRequestDispatcher("listaCanciones.jsp");
+        view.forward(request,response);
 
     }
 }
