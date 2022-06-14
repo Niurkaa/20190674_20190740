60fe3acc55dac579994ec93e8f0599211b222cb7
Niurka Carrión <niurka.carrion@pucp.edu.pe>
2022-06-14 00:10:53 -0500

    bean

--- LAB8_IWEB1_BASE/src/main/java/Beans/Cancion.java
+++ LAB8_IWEB1_BASE/src/main/java/Beans/Cancion.java
@@ -1,7 +1,37 @@
 package Beans;
 
 public class Cancion {
+    private int idCancion;
+    private String nombre_cancion;
+    private String banda;
 
+    public Cancion(int idCancion, String nombre_cancion, String banda ) {
+        this.idCancion = idCancion;
+        this.nombre_cancion=nombre_cancion;
+        this.banda= banda;
+    }
 
+    public int getIdCancion() {
+        return idCancion;
+    }
 
+    public void setIdCancion(int idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public String getNombre_cancion() {
+        return nombre_cancion;
+    }
+
+    public void setNombre_cancion(String nombre_cancion) {
+        this.nombre_cancion = nombre_cancion;
+    }
+
+    public String getBanda() {
+        return banda;
+    }
+
+    public void setBanda(String banda) {
+        this.banda = banda;
+    }
 }
